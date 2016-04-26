e9723a7735e05edb7f8768c5a7dd7a51006e262d
lujun9972 <lujun9972@gmail.com>
2016-04-26 20:27:54 +0800

    Copyright

--- verify-url.el
+++ verify-url.el
@@ -1,6 +1,6 @@
 ;;; verify-url.el --- find out invalid urls in the buffer or region
 
-;; Copyright (C) 2004-2015 Free Software Foundation, Inc.
+;; Copyright (C) 2004-2015 DarkSun <lujun9972@gmail.com>.
 
 ;; Author: DarkSun <lujun9972@gmail.com>
 ;; Created: 2015-12-21
